97d61547067e1199badc225d091a7ec236470679
Bhawesh Chhetri <bhaweshchhetri1@gmail.com>
2017-07-04 10:04:36 -0500

    updated main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,28 @@
 #include "print_folder.h"
 
-int main(){
+int main(int argc, char * argv[]){
+	cout<<__LINE__<<endl;
+	PrintFolder pf;
+	cout<<__LINE__<<endl;
 	struct	kevent event;	 /* Event we want to monitor */
 	struct	kevent tevent;	 /* Event triggered */
 	int kq, ret;
-	g_fd = open(argv[2], O_RDONLY); /* Get the dir from argv */
-	int log_fd = open("files.txt",O_RDWR|O_CREAT,S_IRWXO| S_IRWXG|S_IRWXU);
-	if(log_fd < 0){
-		printf("Error!\n");
-	}
-	if (g_fd == -1){
-		// add this to the err log (along with date)
-	    err(EXIT_FAILURE, "Failed to open '%s'", argv[1]);
-	   	exit(EXIT_FAILURE);
-	}
 	kq = kqueue();
+	DIR* dir = opendir("/Users/Bhawesh/Documents/print_folder");
+	int g_fd = 0;
+	if (dir){
+	    /* Directory exists. */
+	    g_fd = open("/Users/Bhawesh/Documents/print_folder",O_RDONLY);
+	    closedir(dir);
+	}else{
+		g_fd = mkdir("/Users/Bhawesh/Documents/print_folder", S_IRWXU|S_IRWXG|S_IRWXO);
+	}
 	if (kq	== -1){
 		// add this to the err log (along with date)
 	    err(EXIT_FAILURE, "kqueue() failed");
 	   	exit(EXIT_FAILURE);
 	}
+	cout<<"The fd is "<<g_fd<<endl;
 	EV_SET(&event, g_fd, EVFILT_VNODE, EV_ADD|EV_CLEAR, NOTE_WRITE, 0, NULL);	/* Add the folder to the queue (to monitor) */
 	ret = kevent(kq, &event, 1, NULL, 0, NULL);	/* start monitoring the folder */
 	if (ret == -1){
@@ -36,7 +39,9 @@ int main(){
 			// add this to the err log (along with date)
 			err(EXIT_FAILURE, "kevent wait");
 		}else{
-			handle_changes();
+			cout<<"in the main\n";
+			pf.handle_changes();
+			//handle_changes();
 		}
 	}
 }
